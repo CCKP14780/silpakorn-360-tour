0e3933fb04724b522737c9b5208cb07f8ec99ef7
Chitrakorn Phaetlakfa <psserverarted@gmail.com>
2025-12-23 09:03:48 +0700

    upload 360 wang tha phra campus tour
    upload raw 360 app files of wang tha phra campus tour, Silpakorn University

---
+++ wangThaPhraCampus/data.js
@@ -0,0 +1,546 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-map06_p360_wtp001",
+      "name": "MAP06_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.5805753268544844,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5766650421392931,
+          "pitch": -0.010693378751637894,
+          "rotation": 0,
+          "target": "1-map05_p360_wtp001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-map05_p360_wtp001",
+      "name": "MAP05_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.4906337508862375,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8659355471434544,
+          "pitch": 0.03182800015291676,
+          "rotation": 0,
+          "target": "0-map06_p360_wtp001"
+        },
+        {
+          "yaw": -1.6293294561493923,
+          "pitch": -0.0027245780983236045,
+          "rotation": 0,
+          "target": "2-map04_p360_wtp001"
+        },
+        {
+          "yaw": -0.06285366660001301,
+          "pitch": 0.03862433664651732,
+          "rotation": 0,
+          "target": "5-map01_p360_wtp001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8375058033956861,
+          "pitch": -0.1913243732890777,
+          "title": "Amazon Cafe",
+          "text": "Drinks and Beverages"
+        },
+        {
+          "yaw": -2.48934442752204,
+          "pitch": -0.36561588155181113,
+          "title": "SU Central Library",
+          "text": "Library of Silpakorn University, Wang Tha Phra Campus"
+        },
+        {
+          "yaw": -1.043039452441775,
+          "pitch": 0.021403585315770357,
+          "title": "SU Art Centre",
+          "text": "Art Centre in Silpakorn University"
+        },
+        {
+          "yaw": 2.1336207007613393,
+          "pitch": -0.08771243736399725,
+          "title": "Ganesha Statue",
+          "text": "Mascot of Silpakorn University"
+        },
+        {
+          "yaw": -1.8405212051566036,
+          "pitch": -0.02236259943621377,
+          "title": "Office of the President",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Office of the President, Silpakorn University</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-map04_p360_wtp001",
+      "name": "MAP04_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.5611211970592276,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.603380444988531,
+          "pitch": 0.001629050099591467,
+          "rotation": 0,
+          "target": "1-map05_p360_wtp001"
+        },
+        {
+          "yaw": -1.5883625672714494,
+          "pitch": -0.035069009820617225,
+          "rotation": 0,
+          "target": "3-map03_p360_wtp001"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-map03_p360_wtp001",
+      "name": "MAP03_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.674286553871772,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0692955764987655,
+          "pitch": -0.05442414346546087,
+          "rotation": 0,
+          "target": "2-map04_p360_wtp001"
+        },
+        {
+          "yaw": -1.435237487481663,
+          "pitch": -0.02357566181220072,
+          "rotation": 0,
+          "target": "4-map02_p360_wtp001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5165421603112268,
+          "pitch": -0.21203671824054382,
+          "title": "SU Tree",
+          "text": "Main Attraction of the campus"
+        },
+        {
+          "yaw": -0.6332355681158504,
+          "pitch": -0.47477021963595334,
+          "title": "Conference Building",
+          "text": "Conference building, Silpakorn University"
+        },
+        {
+          "yaw": -2.4369607218911344,
+          "pitch": -0.17017636376598944,
+          "title": "Fine Art Building",
+          "text": "Building for faculty of Fine Art"
+        }
+      ]
+    },
+    {
+      "id": "4-map02_p360_wtp001",
+      "name": "MAP02_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": 1.236880539162165,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.518690072520938,
+          "pitch": -0.03860035005146223,
+          "rotation": 0,
+          "target": "3-map03_p360_wtp001"
+        },
+        {
+          "yaw": 2.6376967660568393,
+          "pitch": -0.0033703093952333063,
+          "rotation": 0,
+          "target": "5-map01_p360_wtp001"
+        },
+        {
+          "yaw": 1.2339943444136185,
+          "pitch": -0.0235607536426663,
+          "rotation": 0,
+          "target": "6-map07_p360_wtp001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8217198215874362,
+          "pitch": -0.04735456385126291,
+          "title": "Faculty of Archiology",
+          "text": "Faculty of Archiology, Silpakorn University"
+        },
+        {
+          "yaw": -2.5276089181528185,
+          "pitch": -0.3730744146991327,
+          "title": "Silpa Bhirasri Building",
+          "text": "Silpa Bhirasri Building, Silpakorn University"
+        },
+        {
+          "yaw": -2.8235549634121444,
+          "pitch": -0.03961531611583169,
+          "title": "Yellow Building",
+          "text": "Cafe, Study Hall"
+        }
+      ]
+    },
+    {
+      "id": "5-map01_p360_wtp001",
+      "name": "MAP01_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.6104401634463343,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.175809495571011,
+          "pitch": -0.0007047727061788578,
+          "rotation": 0,
+          "target": "4-map02_p360_wtp001"
+        },
+        {
+          "yaw": 1.9377024020233469,
+          "pitch": 0.004534513330062495,
+          "rotation": 0,
+          "target": "1-map05_p360_wtp001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6180948927332395,
+          "pitch": -0.16441083214022534,
+          "title": "Green Pavilion",
+          "text": "Green Pavilion, Silpakorn University"
+        },
+        {
+          "yaw": 0.3916788609008677,
+          "pitch": 0.24801111939627063,
+          "title": "Statue III",
+          "text": "SU Statue 3"
+        },
+        {
+          "yaw": -1.375594090442938,
+          "pitch": 0.24090267986368374,
+          "title": "Statue II",
+          "text": "SU Statue 2"
+        },
+        {
+          "yaw": -1.8408497905448513,
+          "pitch": 0.14147867128334113,
+          "title": "Statue I",
+          "text": "SU Statue 1"
+        },
+        {
+          "yaw": 2.6771781318806847,
+          "pitch": 0.30629318418451845,
+          "title": "Statue IV",
+          "text": "SU Statue 4"
+        },
+        {
+          "yaw": 2.97558931701726,
+          "pitch": 0.08934962157542081,
+          "title": "Statue V",
+          "text": "SU Statue 5"
+        }
+      ]
+    },
+    {
+      "id": "6-map07_p360_wtp001",
+      "name": "MAP07_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.8239262572367068,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.38356832622864623,
+          "pitch": -0.0027316102396177655,
+          "rotation": 0,
+          "target": "4-map02_p360_wtp001"
+        },
+        {
+          "yaw": -2.6688912762733636,
+          "pitch": 0.014282917196203115,
+          "rotation": 0,
+          "target": "7-map08_p360_wtp001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2584829968056415,
+          "pitch": -0.02829180216346927,
+          "title": "The Gallery of Art and Design Building",
+          "text": "The Gallery of Art and Design Building, Silpakorn University"
+        },
+        {
+          "yaw": 1.6203120290367732,
+          "pitch": -0.059651223163609046,
+          "title": "Faculty of Architecture",
+          "text": "Building for the faculty of architecture"
+        }
+      ]
+    },
+    {
+      "id": "7-map08_p360_wtp001",
+      "name": "MAP08_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -0.1423996365567639,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4270298441554452,
+          "pitch": 0.04315252472469844,
+          "rotation": 0,
+          "target": "6-map07_p360_wtp001"
+        },
+        {
+          "yaw": 2.562478216991888,
+          "pitch": -0.017024886371265424,
+          "rotation": 0,
+          "target": "8-map09_p360_wtp001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0340619774620059,
+          "pitch": 0.04475244848439175,
+          "title": "The Gallery of Art and Design Building",
+          "text": "The Gallery of Art and Design Building, Silpakorn University"
+        }
+      ]
+    },
+    {
+      "id": "8-map09_p360_wtp001",
+      "name": "MAP09_p360_wtp001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1944,
+      "initialViewParameters": {
+        "yaw": -1.6706915465703318,
+        "pitch": 0,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1735968020855552,
+          "pitch": -0.010422205889653924,
+          "rotation": 0,
+          "target": "7-map08_p360_wtp001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4842067228141254,
+          "pitch": -0.21832527795245227,
+          "title": "Silpa Bhirasri Statue",
+          "text": "Statue of Corrado Feroci, Silpakorn University"
+        },
+        {
+          "yaw": 0.18577932006859044,
+          "pitch": 0.01472081176187956,
+          "title": "PSG Art Gallery",
+          "text": "Art Gallery for Faculty of Fine Art, Silpakorn University"
+        }
+      ]
+    }
+  ],
+  "name": "wangThaPhraTour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
